54878f3577394c523b95eb2b4d6abd5507270c39
赵康 <19037900913@163.com>
2024-10-18 13:48:01 +0800

    Initial Commit

---
+++ ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ui
+//
+//  Created by 赵康 on 2024/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ui/uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiApp.swift
+//  ui
+//
+//  Created by 赵康 on 2024/10/17.
+//
+
+import SwiftUI
+
+@main
+struct uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
